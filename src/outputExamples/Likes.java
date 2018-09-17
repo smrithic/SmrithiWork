616132cb0f07da63be1942f3f1b173d9be48b70a
Smrithi <smrithi.chanj@gmail.com>
2018-09-17 09:12:32 -0500

    Likes and Dislikes

--- src/outputExamples/Likes.java
+++ src/outputExamples/Likes.java
@@ -4,11 +4,11 @@ public class Likes {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Likes");
-System.out.println("dogs");
-System.out.println("popcorn");
-System.out.println("movies");
-System.out.println("sleeping");
+System.out.println("Likes|Dislikes");
+System.out.println("dogs|cats");
+System.out.println("popcorn|milk");
+System.out.println("movies|school");
+System.out.println("sleeping|studying");
 
 
 
