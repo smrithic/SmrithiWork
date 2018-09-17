b89abe6dd91281768257410e0f0ba87759942d93
Smrithi <smrithi.chanj@gmail.com>
2018-09-17 08:53:39 -0500

    Class Schedule Project

---
+++ src/outputExamples/SmrithiC.java
@@ -0,0 +1,10 @@
+package outputExamples;
+
+public class SmrithiC {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Class Schedule");
+	}
+
+}
