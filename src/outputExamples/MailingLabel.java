2d0539fa44b75f32f2f1bbb609894279b1ba6fbc
Smrithi <smrithi.chanj@gmail.com>
2018-09-17 09:17:25 -0500

    Mailing Address

---
+++ src/outputExamples/MailingLabel.java
@@ -0,0 +1,13 @@
+package outputExamples;
+
+public class MailingLabel {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Smrithi Chanjeevaram 14220 Lindsay Dr Brookfield WI 53005");
+		System.out.println("John Smith 456 Everywhere Blvd Johnsontown St 45678");
+
+
+	}
+
+}
