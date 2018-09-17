4b23ad8e6c9e0974d27cfd087fc558fa3afc9e5f
Smrithi <smrithi.chanj@gmail.com>
2018-09-17 09:09:17 -0500

    List of Likes

--- src/outputExamples/Likes.java
+++ src/outputExamples/Likes.java
@@ -5,6 +5,14 @@ public class Likes {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Likes");
+System.out.println("dogs");
+System.out.println("popcorn");
+System.out.println("movies");
+System.out.println("sleeping");
+
+
+
+
 	}
 
 }
