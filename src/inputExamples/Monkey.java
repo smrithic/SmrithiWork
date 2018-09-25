9906c7c683a415e93a1f8284e83a6279ece85fa1
Smrithi <smrithi.chanj@gmail.com>
2018-09-25 08:09:17 -0500

    Monkey (2) - re saved

--- src/inputExamples/Monkey.java
+++ src/inputExamples/Monkey.java
@@ -1,14 +1,16 @@
 package inputExamples;
+
 import java.util.Scanner;
+
 public class Monkey {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-double height = 35.7;
-Scanner input = new Scanner(System.in);
-System.out.println("What is the height of the monkey");
-height = input.nextDouble();
-System.out.println("The height of a monkey is " + height);
+		double height = 35.7;
+		Scanner input = new Scanner(System.in);
+		System.out.println("What is the height of the monkey");
+		height = input.nextDouble();
+		System.out.println("The height of a monkey is " + height);
 	}
 
 }
