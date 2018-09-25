3134904b4150a78520acc23a4488d67e7044ee59
Smrithi <smrithi.chanj@gmail.com>
2018-09-25 09:05:52 -0500

    Locker combination

---
+++ src/inputExamples/Lockercombination.java
@@ -0,0 +1,23 @@
+package inputExamples;
+import java.util.Scanner;
+
+public class Lockercombination {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int age = 12;
+int dadAge = 20;
+int momAge = 19;
+Scanner input = new Scanner(System.in);
+System.out.println("how old are you?");
+age = input.nextInt();
+System.out.println("how old is your dad?");
+dadAge = input.nextInt();
+System.out.println("how old is your mom?");
+momAge = input.nextInt();
+System.out.println("your locker combination is " + age + " then " + dadAge + " and " + momAge + " at the end.");
+	input.close();
+
+	}
+
+}
