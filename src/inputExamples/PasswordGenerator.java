494fa0ac41116514d8758ad22bf7bcbb37022582
Smrithi <smrithi.chanj@gmail.com>
2018-09-26 08:13:52 -0500

    Fixed Password generator

--- src/inputExamples/PasswordGenerator.java
+++ src/inputExamples/PasswordGenerator.java
@@ -6,16 +6,16 @@ public class PasswordGenerator {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 int favoriteWholeNumber = 5;
-int houseNumber = 14328;
+int age = 2;
 int numberOfPets = 5;
 Scanner input = new Scanner(System.in);
 System.out.println("What is your favorite whole number?");
 		favoriteWholeNumber = input.nextInt();
-		System.out.println("What is your house number?");
-		houseNumber = input.nextInt();
+		System.out.println("What is your age?");
+		age = input.nextInt();
 		System.out.println("How many pets do you have?");
 		numberOfPets = input.nextInt();
-		System.out.println("your password is " + favoriteWholeNumber + houseNumber + numberOfPets);
+		System.out.println("your password is " + favoriteWholeNumber + age + numberOfPets);
 		input.close();
 	
 	}
