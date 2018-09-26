a458d7de5dac5c1c2d4830819aac761c9b5f9045
Smrithi <smrithi.chanj@gmail.com>
2018-09-26 09:04:16 -0500

    Voting

--- src/mathOperators/Vote.java
+++ src/mathOperators/Vote.java
@@ -1,10 +1,18 @@
 package mathOperators;
 
+import java.util.Scanner;
+
 public class Vote {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int age = 0;
+		Scanner input = new Scanner(System.in);
+		System.out.println("how old are you?");
+		age = input.nextInt();
+		age = 18 - age;
+		System.out.println("You can vote in " + age + " years.");
+		input.close();
 	}
 
 }
