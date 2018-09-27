fe3ea6a63046a0be2dca632c3693a7e92861ac39
Smrithi <smrithi.chanj@gmail.com>
2018-09-27 08:51:39 -0500

    Area of Circle
    Edited the pi. It should have been in all caps since it was a constant

--- src/mathOperators/AreaofaCirlce.java
+++ src/mathOperators/AreaofaCirlce.java
@@ -7,12 +7,12 @@ public class AreaofaCirlce {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		double radius = 0.0;
-		final double pi = 3.14;
+		final double PI = 3.14;
 		double area = 0.0;
 		Scanner input = new Scanner(System.in);
 		System.out.println("What is the radius of the circle?");
 		radius = input.nextDouble();
-		area = radius * radius * pi;
+		area = radius * radius * PI;
 		System.out.println("The area of the circle is " + area);
 		input.close();
 
