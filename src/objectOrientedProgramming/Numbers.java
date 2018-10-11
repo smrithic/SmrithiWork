d2f04c6919e70aa48f14dd8f301318aef834a5e5
Smrithi <smrithi.chanj@gmail.com>
2018-10-11 08:51:52 -0500

    Parameter Examples

---
+++ src/objectOrientedProgramming/Numbers.java
@@ -0,0 +1,24 @@
+package objectOrientedProgramming;
+
+public class Numbers {
+
+	public void sayNumber(double num) {
+	}
+
+	public Numbers() {
+	}
+
+	public void sayNumber(int num) {
+		System.out.println(num);
+	}
+
+	public void sayNumberPlus2(double num) {
+		System.out.println(num+2);
+	}
+	
+	public void saySum(double numone, double numtwo) {
+	double sum = numone + numtwo;
+	System.out.println(sum);
+	}
+	
+}
---
+++ src/objectOrientedProgramming/NumbersTestClass.java
@@ -0,0 +1,14 @@
+package objectOrientedProgramming;
+
+public class NumbersTestClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Numbers bob = new Numbers();
+		bob.sayNumber(3);
+		Numbers joe = new Numbers();
+		joe.sayNumberPlus2(4);
+		bob.saySum(3.0, 4.0);
+	}
+
+}
