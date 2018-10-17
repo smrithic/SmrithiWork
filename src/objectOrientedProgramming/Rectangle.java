19307d63e1b82a1a93f1c46d1f39d9bb421826e7
Smrithi <smrithi.chanj@gmail.com>
2018-10-17 09:01:07 -0500

    Rectangle

---
+++ src/objectOrientedProgramming/Rectangle.java
@@ -0,0 +1,35 @@
+package objectOrientedProgramming;
+
+public class Rectangle {
+	private double length = 0.0;
+	private double width = 0.0;
+	private double perimeter = 0.0;
+	private double area = 0.0;
+
+	public Rectangle() {
+		length = 4.5;
+		width = 3.5;
+		area=15.75;
+		perimeter=16;
+	}
+
+	public double getLength() {
+		return length;
+	}
+
+	public double getWidth() {
+		return width;
+	}
+	public double getPerimeter() {
+		return perimeter;
+	}
+	public double getArea() {
+		return area;
+	}
+	public void changeLength(double newL) {
+		length = newL;
+	}
+	public void changeWidth(double newW) {
+		width = newW;
+	}
+}
---
+++ src/objectOrientedProgramming/RectangleTestClass.java
@@ -0,0 +1,16 @@
+package objectOrientedProgramming;
+
+public class RectangleTestClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Rectangle bob = new Rectangle();
+bob.changeLength(7.9);
+bob.changeWidth(10.8);
+bob.getPerimeter();
+bob.getArea();
+System.out.println(bob.getPerimeter());
+System.out.println(bob.getArea());
+	}
+
+}
