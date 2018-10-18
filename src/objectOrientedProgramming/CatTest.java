a33fdee95642932d1534436992a7a5b7a55fb5a4
Smrithi <smrithi.chanj@gmail.com>
2018-10-18 08:28:32 -0500

    Cat Class

---
+++ src/objectOrientedProgramming/CatTest.java
@@ -0,0 +1,17 @@
+package objectOrientedProgramming;
+
+public class CatTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Cat fur = new Cat();
+fur.getAge();
+fur.getSleep();
+fur.changeBirthday(45);
+fur.sleepMore(5);
+fur.sleepLess(4);
+System.out.println(fur.getAge());
+System.out.println(fur.getSleep());
+	}
+
+}
