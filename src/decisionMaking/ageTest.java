98e0333f930508fde1fbce25e281c4181a8bdc18
Smrithi <smrithi.chanj@gmail.com>
2018-10-24 09:08:21 -0500

    age

---
+++ src/decisionMaking/ageTest.java
@@ -0,0 +1,17 @@
+package decisionMaking;
+import java.util.Scanner;
+public class ageTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int age = 0;
+Scanner input = new Scanner(System.in);
+System.out.println("What is your age?");
+age = input.nextInt();
+System.out.println(age==16);
+
+
+
+	}
+
+}
