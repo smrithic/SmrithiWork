ff2f784c36d8a29f89f583c519a16a05ed199bda
Smrithi <smrithi.chanj@gmail.com>
2018-10-25 08:54:57 -0500

    age
    if you're older than 18, you will be charged as an adult. If not, then have a nice day.

--- src/decisionMaking/ageTest.java
+++ src/decisionMaking/ageTest.java
@@ -9,8 +9,11 @@ Scanner input = new Scanner(System.in);
 System.out.println("What is your age?");
 age = input.nextInt();
 System.out.println(age==16);
-
-
+if(age >= 18)
+{
+	System.out.println("You will be charged as an adult");
+}
+System.out.println("Have a nice day");
 
 	}
 
