93ad2ba4fd5308ed61b7873c0c1cc0dd56f8775e
Smrithi <smrithi.chanj@gmail.com>
2018-11-01 01:19:22 -0500

    Grade average

--- src/decisionMaking/AgeDecision.java
+++ src/decisionMaking/AgeDecision.java
@@ -1,20 +1,18 @@
 package decisionMaking;
-
 import java.util.Scanner;
-
 public class AgeDecision {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int age;
-		Scanner input = new Scanner(System.in);
-		System.out.println("What is your age?");
-		age = input.nextInt();
-		if (age <= 17) {
-			System.out.println("You are getting an A in this class");
-		}
-		System.out.println("Have a great day!");
-		input.close();
+int age;
+Scanner input = new Scanner(System.in);
+System.out.println("What is your age?");
+age = input.nextInt();
+if (age <= 17) {
+	System.out.println("You are getting an A in this class");
+}
+System.out.println("Have a great day!");
+input.close();
 	}
 
 }
