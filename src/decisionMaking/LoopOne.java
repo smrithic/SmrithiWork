e909c50b9134189807479435210484c80015e746
Smrithi <smrithi.chanj@gmail.com>
2018-11-05 08:40:19 -0600

    Loop in class activity 3

--- src/decisionMaking/LoopOne.java
+++ src/decisionMaking/LoopOne.java
@@ -1,15 +1,20 @@
 package decisionMaking;
 
+import java.util.Scanner;
+
 public class LoopOne {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
 		int count = 0;
-		while (count <= 100000) {
+		int multiple;
+		System.out.println("Until what number should we do multiples of?");
+		multiple = input.nextInt();
+		while (count <= multiple) {
 			System.out.println(count);
-			count= count + 2;
+			count = count + 2;
 		}
-		
 
 	}
 
