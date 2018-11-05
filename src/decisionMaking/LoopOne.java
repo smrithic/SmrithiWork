a69f978cb029563873aabbd04f59ca736cf4c79b
Smrithi <smrithi.chanj@gmail.com>
2018-11-05 08:36:21 -0600

    Loop activity 2

--- src/decisionMaking/LoopOne.java
+++ src/decisionMaking/LoopOne.java
@@ -5,9 +5,9 @@ public class LoopOne {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int count = 0;
-		while (count <= 100) {
+		while (count <= 100000) {
 			System.out.println(count);
-			count++;
+			count= count + 2;
 		}
 		
 
