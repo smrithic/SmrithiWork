3b9ba4ee6506a8d9f14f8f03ac40fa51868e9c8a
Smrithi <smrithi.chanj@gmail.com>
2018-11-05 08:31:43 -0600

    LoopOne

---
+++ src/decisionMaking/LoopOne.java
@@ -0,0 +1,16 @@
+package decisionMaking;
+
+public class LoopOne {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int count = 0;
+		while (count <= 100) {
+			System.out.println(count);
+			count++;
+		}
+		
+
+	}
+
+}
