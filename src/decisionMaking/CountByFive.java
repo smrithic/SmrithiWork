b403191cdc1112185f388d2986dbff2ddbd7a001
Smrithi <smrithi.chanj@gmail.com>
2018-11-07 18:12:38 -0600

    Count by 5

---
+++ src/decisionMaking/CountByFive.java
@@ -0,0 +1,19 @@
+package decisionMaking;
+
+public class CountByFive {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int number = 49;
+		int multiple = 0;
+		while (multiple < number) {
+
+			if (multiple < number) {
+				System.out.println(multiple);
+
+			}
+			multiple += 5;
+		}
+
+	}
+}
