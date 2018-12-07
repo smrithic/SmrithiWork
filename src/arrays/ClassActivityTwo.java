588d80ae5e5669f231c4001eb237f7eaff001576
Smrithi <smrithi.chanj@gmail.com>
2018-12-07 08:46:38 -0600

    formatting for second class activity

--- src/arrays/ClassActivityTwo.java
+++ src/arrays/ClassActivityTwo.java
@@ -6,20 +6,20 @@ public class ClassActivityTwo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-Scanner input = new Scanner(System.in);
-final int maxStudents = 5;
-double highestTest = 0;
-double [] scores = new double[maxStudents];
-for (int i = 0; i < maxStudents; i++) {
-	System.out.println("Enter a test score");
-	scores[i] = input.nextDouble();
-	if (highestTest < scores[i]) {
-		highestTest = scores[i];
-	}
-}
-System.out.println("The largest score is " + highestTest);
-
-
+		Scanner input = new Scanner(System.in);
+		
+		final int maxStudents = 5;
+		double highestTest = 0;
+		double[] scores = new double[maxStudents];
+		
+		for (int i = 0; i < scores.length; i++) {
+			System.out.println("Enter a test score");
+			scores[i] = input.nextDouble();
+			if (highestTest < scores[i]) {
+				highestTest = scores[i];
+			}
+		}
+		System.out.println("The largest score is " + highestTest);
 
 	}
 
