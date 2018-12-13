603d83ee4d8a25bb0253d9b3a067b8ceee5d9ece
Smrithi <smrithi.chanj@gmail.com>
2018-12-12 20:06:56 -0600

    Test prep 2
    asking the user for 10 test scores, storing them in an array, and displaying them in reverse order

--- src/arrays/Practice1.java
+++ src/arrays/Practice1.java
@@ -7,13 +7,15 @@ public class Practice1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner input = new Scanner(System.in);
-		double [] scores = new double[5];
+		double [] scores = new double[10];
 //prompt the user for 5 test scores
-		for (int i = 0; i < 5; i++) {
+		for (int i = 0; i < 10; i++) {
 			System.out.println("Please enter a test score");
 			scores[i] = input.nextDouble();
 		}
-		
+		for (int i = scores.length - 1; i>= 0; i--) {
+		System.out.println("The scores in reverse order are: " + scores[i]);
+		}
 	}
 
 }
