dbb34282ef75c9c728f806156410405c220a80a3
Smrithi <smrithi.chanj@gmail.com>
2019-01-07 08:57:47 -0600

    In class activtiy 3

--- src/arrayLists/InClass1.java
+++ src/arrayLists/InClass1.java
@@ -1,25 +1,20 @@
 package arrayLists;
 
 import java.util.ArrayList;
-import java.util.Scanner;
-import java.util.ArrayList;
+
+import objectOrientedProgramming.Friend;
+
 
 public class InClass1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner input = new Scanner(System.in);
 		
-		System.out.println("Please enter your name");
-		String name = input.nextLine();
-		
-		ArrayList<String> people = new ArrayList<String>();
-		people.add("Bob");
-		people.add("Jane");
-		people.add(1, "Billy");
-		people.add(name);
-		
-		System.out.println(people);
+		ArrayList<Friend>people = new ArrayList<Friend>();
+		people.add(new Friend("bob", 35));
+		people.add(0, new Friend("jane", 4));
+		people.add(0, new Friend("billy", 75));
+
 	}
 
 }
