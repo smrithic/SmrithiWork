8ddc0c2cb2553169e334b0e51690ec6dacfaef65
Smrithi <smrithi.chanj@gmail.com>
2019-01-08 08:33:13 -0600

    In Class activity 7

--- src/arrayLists/InClass1.java
+++ src/arrayLists/InClass1.java
@@ -1,25 +1,28 @@
 package arrayLists;
 
 import java.util.ArrayList;
-
-import objectOrientedProgramming.Friend;
-
-
+import java.util.Scanner;
 
 public class InClass1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		ArrayList<Friend>people = new ArrayList<Friend>();
-		people.add(new Friend("bob", 35));
-		people.add(0, new Friend("jane", 4));
-		people.add(0, new Friend("billy", 75));
-		for(int i = 0; i < people.size(); i ++) {
-			Friend buddy = people.get(i);
-			System.out.println(buddy.getName());
-		}
 
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("Please enter your name");
+		String name = input.nextLine();
+
+		ArrayList<String> people = new ArrayList<String>();
+		people.add("Bob");
+		people.add("Jane");
+		people.add(1, "Billy");
+		people.add(name);
+		people.remove(0);
+		people.remove(people.size() - 1);
+		for (int i = 0; i < people.size(); i++) {
+			System.out.println(people.get(i));
+		}
 	}
 
 }
