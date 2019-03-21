11e45da9aaadf9af494d6e529e1b09866054df89
Smrithi Chanjeevaram <smrithi.chanj@gmail.com>
2019-03-21 08:41:44 -0500

    Student Teacher test class

--- src/interfaces/StudentTeacherTest.java
+++ src/interfaces/StudentTeacherTest.java
@@ -1,5 +1,7 @@
 package interfaces;
+
 import java.util.ArrayList;
+
 public class StudentTeacherTest {
 
 	public static void main(String[] args) {
@@ -26,24 +28,55 @@ public class StudentTeacherTest {
 
 		// Using the person interface, display the name and age of the teacher and
 		// student
-		Person peep1 = adult;
-		System.out.println("The name of the teacher is: " + peep1.getName());
-		System.out.println("The age of the teacher is: " + peep1.getAge());
+		Person someone = adult;
+		System.out.println("The name of the teacher is: " + someone.getName());
+		System.out.println("The age of the teacher is: " + someone.getAge());
 
-		peep1 = kid;
-		System.out.println("The name of the student is: " + peep1.getName());
-		System.out.println("The name of the teacher is: " + peep1.getName());
-		
+		someone = kid;
+		System.out.println("The name of the student is: " + someone.getName());
+		System.out.println("The name of the teacher is: " + someone.getName());
+
 		Teacher adult2 = new Teacher();
 		Student kid2 = new Student();
-		ArrayList<Person>peep = new ArrayList<Person>();
+		ArrayList<Person> peep = new ArrayList<Person>();
 		peep.add(kid);
 		peep.add(kid2);
 		peep.add(adult);
 		peep.add(adult2);
-		
-		for(Person p: peep) {
+
+		for (Person p : peep) {
 			System.out.println(p.getName());
+		}
+		// displaying the ages of the people that have aname containing more than 10
+		// printable characters
+		for (Person p : peep) {
+			if (p.getName().length() > 10) {
+				System.out.println(p.getAge());
+			}
+		}
+		// make students out sick using the interface and display the names
+		for (Person p : peep) {
+			if (someone instanceof Student) {
+				Student bob = (Student) someone;
+				bob.outSick();
+
+			}
+		}
+		for (Person p : peep) {
+			System.out.println(p.getName());
+		}
+
+		// pay each teacher and display names of students in array list
+		for (Person p : peep) {
+			if (someone instanceof Teacher) {
+				Teacher john = (Teacher) someone;
+				john.payMe();
+			}
+		}
+		for (Person p : peep) {
+			if (someone instanceof Student) {
+				System.out.println(p.getName());
+			}
 		}
 
 	}
